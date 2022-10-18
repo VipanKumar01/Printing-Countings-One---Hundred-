795348caa143ba90f9e51924de75c584d78def81
Vipan Kumar <108624219+VipanKumar01@users.noreply.github.com>
2022-10-18 01:45:16 -0700

    Update Printing (1-100)Code Resolve.py

--- Printing (1-100)Code Resolve.py
+++ Printing (1-100)Code Resolve.py
@@ -1,3 +1,6 @@
+# Name = Vipan Kumar
+# GitHub user name = @VipanKumar01
+
 a = ['','One','Two','Three','Four','Five','Six','Seven','Eight','Nine']
 b = ['Ten','Eleven','Twelve','Thirteen','Fourteen','Fifteen','Sixteen','Seventeen','Eighteen','Nineteen']
 c = ['Twenty','Thirty','Forty','Fifty','Sixty','Seventy','Eighty','Ninety','One Hundred']
@@ -7,4 +10,6 @@ for i in range(n+1):
     if i<=9:                    print(a[i])
     if i>=10 and i<=19:         print(b[temp1])
     if i>=20 and i<=100:         print(c[temp2-2],a[temp1])
-# if n<=100: print('One Hundred')
+
+        
+#  --HappyCode--
